3d96634ecb191bbd3a8fd76eaee2d9e87b0b69e4
kbarkowski <karol.barkowski@gmail.com>
2017-04-24 00:05:58 +0200

    animation moved to menu child items + delay added

--- src/menu.js
+++ src/menu.js
@@ -1,37 +1,25 @@
 import React from 'react';
 import { View, StyleSheet, Animated } from 'react-native';
-import { transformOrigin, rotateY } from './martixUtils';
+
 import MenuItem from './menuItem';
 
 export default class Menu extends React.Component {
     constructor(props) {
         super(props);
 
-        this.rotY = new Animated.Value(0);
         this.state = {
             isOpened: this.props.isOpened || true
         }
-    }
 
-    componentDidMount() {
-        this.rotY.addListener(({ value }) => {
-            const y = this.rotY.__getValue();
 
-            const origin = { x: -60, y: 0, z: 0 };
-            let matrix = rotateY(y);
-            transformOrigin(matrix, origin);
-
-            this.menu.setNativeProps({ style: { transform: [{ perspective: 1000 }, { matrix: matrix }] } });
-        });
     }
 
     startAnimation(_toValue) {
-        Animated.timing(
-            this.rotY,
-            {
-                toValue: _toValue,
-                duration: 150
-            }).start();
+        let _self = this;
+
+        this.children.forEach(function (child, index) {
+            this.refs[child.ref].show(_toValue, index * 50);
+        }, this);
     }
 
     show() {
@@ -49,10 +37,14 @@ export default class Menu extends React.Component {
     }
 
     render() {
-        return (<View style={styles.menu}
-            ref={component => this.menu = component}
-        >
-            {this.props.children}
+        this.children = this.props.children.map((child, index) => {
+            return React.cloneElement(child, {
+                ref: 'item' + index,
+                key: 'item' + index
+            });
+        });
+        return (<View style={styles.menu} ref={component => this.menu = component}>
+            {this.children}
         </View>);
     }
 }
--- src/menuItem.js
+++ src/menuItem.js
@@ -1,11 +1,44 @@
 import React from 'react';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import { View, Text, StyleSheet } from 'react-native';
+import { transformOrigin, rotateY } from './martixUtils';
+import { View, Text, StyleSheet, Animated } from 'react-native';
 
 export default class MenuItem extends React.Component {
+    constructor(props) {
+        super(props);
+        this.rotY = new Animated.Value(0);
+    }
+
+    show(_toValue, _delay) {
+        Animated.timing(
+            this.rotY,
+            {
+                toValue: _toValue,
+                duration: 150,
+                delay: _delay
+            }).start();
+    }
+
+    hide() {
+
+    }
+
+    componentDidMount() {
+        this.rotY.addListener(({ value }) => {
+            const y = this.rotY.__getValue();
+
+            const origin = { x: -60, y: 0, z: 0 };
+            let matrix = rotateY(y);
+            transformOrigin(matrix, origin);
+
+            this.menuItem.setNativeProps({ style: { transform: [{ perspective: 800 }, { matrix: matrix }] } });
+        });
+    }
+
+
     render() {
         return (
-            <View style={styles.boxContainer}>
+            <View style={styles.boxContainer} ref={component => this.menuItem = component}>
                 <View style={styles.box}>
                     <Icon name="rocket" text="sample icon" size={30} color="#fff" />
                     <Text style={styles.text}>Icon Text</Text>
